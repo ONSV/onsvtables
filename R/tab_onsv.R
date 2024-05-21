e905709d0709fb14684e6f9be7e3eb9d5baa3862
Jota Saraiva <jpsaraiva2002@gmail.com>
2024-05-21 11:52:04 -0300

    Adds docu

--- R/tab_onsv.R
+++ R/tab_onsv.R
@@ -1,16 +1,35 @@
+#' Add styles to a `{gt}` table.
+#'
+#' `tab_onsv()` uses `gt::tab_*()` functions to customize a `gt_tbl` object.
+#'
+#' @param data A `gt_tbl` object.
+#'
+#' @return A `gt_tbl` customized with `tab_style()` and `tab_options()`.
+#' @export
+#'
+#' @examples
+#' df <- data.frame(
+#'   var1 = c("lorem", "ipsum", "dolor"),
+#'   var2 = c(1, 2, 3)
+#' )
+#'
+#' tbl_onsv <-
+#'   df |>
+#'   gt::gt() |>
+#'   tab_onsv()
 tab_onsv <- function(data) {
-  data |> 
+  data |>
     gt::tab_style(
       style = gt::cell_text(align = "center"),
-      locations = gt::cells_stubhead()) |> 
+      locations = gt::cells_stubhead()) |>
     gt::tab_style(
       style = "vertical-align:middle",
       locations = gt::cells_column_labels()
-    ) |> 
+    ) |>
     gt::tab_style(
       style = "vertical-align:middle",
       locations = gt::cells_row_groups()
-    ) |> 
+    ) |>
     gt::tab_options(
       column_labels.background.color = "#00496d",
       column_labels.font.weight = "bold",
